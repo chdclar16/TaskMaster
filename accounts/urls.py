62b8284ece748ce2fdf41055d65d95e0c46d3544
Chad Manuel <chadc_manuel@yahoo.com>
2023-03-06 15:50:20 -0800

    Feature 10 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from accounts.views import login_user, logout_user
+from accounts.views import login_user, logout_user, signup_user
 
 urlpatterns = [
     path("login/", login_user, name="login"),
     path("logout/", logout_user, name="logout"),
+    path("signup/", signup_user, name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from accounts.forms import LoginForm
+from accounts.forms import LoginForm, SignupForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 
 def login_user(request):
@@ -29,3 +30,29 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return redirect("login")
+
+
+def signup_user(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            password_confirmation = form.cleaned_data["password_confirmation"]
+
+            if password == password_confirmation:
+                user = User.objects.create_user(
+                    username,
+                    password=password,
+                )
+                login(request, user)
+                return redirect("list_projects")
+            else:
+                form.add_error("password", "the passwords do not match")
+    else:
+        form = SignupForm()
+
+    context = {
+        "form": form,
+    }
+    return render(request, "registration/signup.html", context)
